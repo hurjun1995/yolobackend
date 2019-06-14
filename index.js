e0e6770f83e37daf8ce95bddbdb137d7f4d60258
hurjun1995 <hurjun1995@gmail.com>
2019-06-14 17:31:08 +0900

    port changed to 8080

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const http = require('http');
 const app = require('./app');
 const CONFIG = require('./config/config');
 
-const port = parseInt(CONFIG.port, 10) || 3000;
+const port = parseInt(CONFIG.port, 10);
 app.set('port', port);
 
 /**
  * Create HTTP server
  */
 const server = http.createServer(app);
-console.log(`listening to ${port}...`);
+console.log(`Listening on ${port}...`);
 server.listen(port);
