af462293d02d92fd6a6db112a3c2f31702063339
Joon Hur <hurjun1995@gmail.com>
2019-08-30 23:09:50 -0700

    removed unused comments

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-until mysql -h db -u joon_admin -pDevUser123!# -e 'select 1'; do echo "still waiting for mysql"; sleep 1; done
-
-# MYSQL=`which mysql`
-# Q1="CREATE DATABASE IF NOT EXISTS $1"
+# until mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} -e 'select 1';
+until mysql -h db -u joon_admin -pDevUser123!# -e 'select 1';
+do echo "still waiting for mysql"; sleep 1;
+done
 
 exec yarn dev
